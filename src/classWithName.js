5734cf2f642d6fe6f1338d33ddb773b1597d54f6
rbalicki2 <robert@hometeamcare.com>
2017-04-13 14:45:01 -0400

    set up lint

--- src/classWithName.js
+++ src/classWithName.js
@@ -1,3 +1,8 @@
-export default (name, parent) => {
-  return Function(`return function ${name}() { parent.apply(this, arguments); }`)();
+/* eslint-disable no-new-func */
+export default (name, parentClass) => {
+  if (parentClass) {
+    return Function(`return function ${name}() { parentClass.apply(this, arguments); }`)();
+  }
+
+  return Function(`return function${name}() {}`)();
 };
